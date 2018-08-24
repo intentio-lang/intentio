ea63a13795942e579654d33a4ef6012ba7c4c6fd
Marek Kaput <marek.kaput@outlook.com>
2018-08-24 15:40:13 +0200

    show error cause in HIR loader

--- src/intentio-hir/src/Intentio/Hir/Compiler.hs
+++ src/intentio-hir/src/Intentio/Hir/Compiler.hs
@@ -8,18 +8,22 @@ where
 
 import           Intentio.Prelude
 
-import           Data.Aeson                     ( decodeStrict'
-                                                , decodeFileStrict'
+import           Data.Aeson                     ( eitherDecodeStrict'
+                                                , eitherDecodeFileStrict'
                                                 )
 
 import           Intentio.Compiler              ( Assembly
                                                 , mapModulesM
                                                 , Compile
                                                 , CompilePure
+                                                , CompileT
                                                 , liftIOE
-                                                , pushDiagnostic
+                                                , pushDiagnostics
                                                 )
-import           Intentio.Diagnostics           ( cerrorFor )
+import           Intentio.Diagnostics           ( HasSourcePos
+                                                , cerrorFor
+                                                , cnoteFor
+                                                )
 import           Language.Intentio.Compiler     ( SourceFile(..)
                                                 , SourceText(..)
                                                 , sourceFilePath
@@ -32,14 +36,20 @@ readHirDumpFiles :: Assembly SourceFile -> Compile (Assembly Module)
 readHirDumpFiles = mapModulesM readHirDumpFile
 
 readHirDumpFile :: SourceFile -> Compile Module
-readHirDumpFile f = liftIOE (decodeFileStrict' (f ^. sourceFilePath)) >>= \case
-  Just m  -> return m
-  Nothing -> pushDiagnostic (cerrorFor f "Invalid HIR dump") >> unreachable
+readHirDumpFile f =
+  liftIOE (eitherDecodeFileStrict' (f ^. sourceFilePath)) >>= \case
+    Right m -> return m
+    Left  e -> err f e
 
 readHirDumpTexts :: Assembly SourceText -> CompilePure (Assembly Module)
 readHirDumpTexts = mapModulesM readHirDumpText
 
 readHirDumpText :: SourceText -> CompilePure Module
-readHirDumpText t = case decodeStrict' (t ^. sourceTextContent & toS) of
-  Just m  -> return m
-  Nothing -> pushDiagnostic (cerrorFor t "Invalid HIR dump") >> unreachable
+readHirDumpText t = case eitherDecodeStrict' (t ^. sourceTextContent & toS) of
+  Right m -> return m
+  Left  e -> err t e
+
+err :: (HasSourcePos t, Monad m) => t -> String -> CompileT m a
+err t e = do
+  pushDiagnostics [cerrorFor t "Invalid HIR dump.", cnoteFor t $ toS e]
+  unreachable

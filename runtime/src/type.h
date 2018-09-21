e9826e9e3cb175663520945b74205019af1cb644
Marek Kaput <marek.kaput@outlook.com>
2018-09-21 10:06:11 +0200

    improve docs

--- runtime/src/type.h
+++ runtime/src/type.h
@@ -18,102 +18,124 @@ typedef IeoResult(IeoOpBinary)(IEO_NOTNULL const IeoTerm *self,
 typedef struct IeoType
 {
   /**
-   * @brief Type name. Preferably the term should be static if declared by
+   * Type name. Preferably the term should be static if declared by
    * native code.
    */
   IEO_NOTNULL IeoString *type_name;
 
   /**
-   * @brief Size of term data, including header, in bytes.
+   * Size of term data, including header, in bytes.
    *
    * This is the size used by ::ieo_term_alloc & family functions.
    */
   size_t term_size;
 
   /**
-   * @brief Free term memory after itself.
+   * Free term memory after itself.
    *
    * In 90% cases, this would point to the ::ieo_term_deleter function.
-   * Overriding it is useful if the term contains owned memory. This method
-   * should either be or call directly or indirectly the mentioned
-   * ::ieo_term_deleter function, which calls `ieo_free()` on the term itself
-   * and also may apply some debug or performance hooks.
+   * Overriding it is useful if the term contains owned memory (to free it) or
+   * other terms (to unref them). This method should either be or call directly
+   * or indirectly the mentioned ::ieo_term_deleter function, which calls
+   * `ieo_free()` on the term itself and also may apply some debug or
+   * performance hooks.
    */
   IEO_NOTNULL void (*deleter)(IEO_NULLABLE IeoTerm *self);
 
   /**
-   * @brief Perform `-self` operation.
+   * @defgroup IeoTypeMethodPointers Method pointers
+   * @{
+   */
+
+  /**
+   * @defgroup IeoTypeMathMethods Mathematical operators implementations
+   * @{
+   */
+
+  /**
+   * Perform `-self` operation.
    */
   IeoOpUnary *neg_func;
 
   /**
-   * @brief Perform `self + rhs` operation.
+   * Perform `self + rhs` operation.
    */
   IeoOpBinary *add_func;
 
   /**
-   * @brief Perform `self / rhs` operation.
+   * Perform `self / rhs` operation.
    */
   IeoOpBinary *div_func;
 
   /**
-   * @brief Perform `self == rhs` operation.
-   *
-   * If this method is not defined, the IeoType::compare_func method is tried.
-   */
-  IeoOpBinary *eq_func;
-
-  /**
-   * @brief Perform `self > rhs` operation.
-   *
-   * If this method is not defined, the IeoType::compare_func method is tried.
-   */
-  IeoOpBinary *gt_func;
-
-  /**
-   * @brief Perform `self >= rhs` operation.
-   *
-   * If this method is not defined, the IeoType::compare_func method is tried.
-   */
-  IeoOpBinary *gteq_func;
-
-  /**
-   * @brief Perform `self < rhs` operation.
-   *
-   * If this method is not defined, the IeoType::compare_func method is tried.
-   */
-  IeoOpBinary *lt_func;
-
-  /**
-   * @brief Perform `self <= rhs` operation.
-   *
-   * If this method is not defined, the IeoType::compare_func method is tried.
-   */
-  IeoOpBinary *lteq_func;
-
-  /**
-   * @brief Perform `self * rhs` operation.
-   *
-   * If this method is not defined, the IeoType::compare_func method is tried.
+   * Perform `self * rhs` operation.
    */
   IeoOpBinary *mul_func;
 
   /**
-   * @brief Perform `self != rhs` operation.
+   * Perform `self - rhs` operation.
+   */
+  IeoOpBinary *sub_func;
+
+  /** @} */
+
+  /**
+   * @defgroup IeoTypeCompareMethods Comparison operators implementations
+   * @{
+   */
+
+  /**
+   * Perform `self == rhs` operation.
+   */
+  IeoOpBinary *eq_func;
+
+  /**
+   * Perform `self > rhs` operation.
    *
-   * If this method is not defined, the IeoType::compare_func method is tried.
+   * Comparison is always performed between terms of same type. If both hand
+   * sides have different type, the Term Ordering by Type rule dictates the
+   * result of the operation.
+   */
+  IeoOpBinary *gt_func;
+
+  /**
+   * Perform `self >= rhs` operation.
+   *
+   * Comparison is always performed between terms of same type. If both hand
+   * sides have different type, the Term Ordering by Type rule dictates the
+   * result of the operation.
+   */
+  IeoOpBinary *gteq_func;
+
+  /**
+   * Perform `self < rhs` operation.
+   *
+   * Comparison is always performed between terms of same type. If both hand
+   * sides have different type, the Term Ordering by Type rule dictates the
+   * result of the operation.
+   */
+  IeoOpBinary *lt_func;
+
+  /**
+   * Perform `self <= rhs` operation.
+   *
+   * Comparison is always performed between terms of same type. If both hand
+   * sides have different type, the Term Ordering by Type rule dictates the
+   * result of the operation.
+   */
+  IeoOpBinary *lteq_func;
+
+  /**
+   * Perform `self != rhs` operation.
    */
   IeoOpBinary *neq_func;
 
   /**
-   * @brief Perform `self - rhs` operation.
+   * Compare `self` to `rhs`.
    *
-   * If this method is not defined, the IeoType::compare_func method is tried.
-   */
-  IeoOpBinary *sub_func;
-
-  /**
-   * @brief Compare `self` to `rhs`.
+   * Comparison is always performed between terms of same type. If both hand
+   * sides have different type, the Term Ordering by Type rule dictates the
+   * result of the operation.
    *
    * Returns:
    * - Negative value if lhs appears before rhs.
@@ -123,10 +145,14 @@ typedef struct IeoType
    * @return IeoInt*
    */
   IeoOpBinary *compare_func;
+
+  /** @} */
+
+  /** @} */
 } IeoType;
 
 /**
- * @brief Iterate over all registered types.
+ * Iterate over all registered types.
  *
  * @param opaque a pointer where the iterator function  will store its state.
  *               Must point to NULL to start the iteration.
@@ -137,7 +163,7 @@ IEO_NULLABLE IeoType *
 ieo_type_iterate(IEO_NOTNULL void **opaque);
 
 /**
- * @brief Compare two types in terms of type sorting order.
+ * Compare two types in terms of type sorting order.
  *
  * Returns:
  * - Negative value if lhs appears before rhs.

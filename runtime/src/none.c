c6a7f3d9f6ddb275a7fee20279f70849d480354b
Marek Kaput <marek.kaput@outlook.com>
2018-09-21 10:20:59 +0200

    cleanups

--- runtime/src/none.c
+++ runtime/src/none.c
@@ -30,60 +30,35 @@ ieo_is_none(IEO_NOTNULL const IeoTerm *term)
   return IEO_BOOL(term->head.ty == &ieo_std_type_none);
 }
 
-IEO_PURE IeoResult
-ieo_none_neg(IEO_NOTNULL const IeoTerm *self)
+static IEO_PURE IeoResult
+unary_math_method(const IeoTerm *self)
 {
   ieo_assert(IEO_OK(ieo_is_none(self)));
   return ieo_none();
 }
 
-IEO_PURE IeoResult
-ieo_none_add(IEO_NOTNULL const IeoTerm *self, IEO_NOTNULL const IeoTerm *other)
-{
-  ieo_assert(IEO_OK(ieo_is_none(self)));
-  IEO_TRY_(ieo_is_none(other));
-  return ieo_none();
-}
-
-IEO_PURE IeoResult
-ieo_none_div(IEO_NOTNULL const IeoTerm *self, IEO_NOTNULL const IeoTerm *other)
+static IEO_PURE IeoResult
+binary_math_method(const IeoTerm *self, const IeoTerm *other)
 {
   ieo_assert(IEO_OK(ieo_is_none(self)));
   IEO_TRY_(ieo_is_none(other));
   return ieo_none();
 }
 
-IEO_PURE IeoResult
-ieo_none_mul(IEO_NOTNULL const IeoTerm *self, IEO_NOTNULL const IeoTerm *other)
-{
-  ieo_assert(IEO_OK(ieo_is_none(self)));
-  IEO_TRY_(ieo_is_none(other));
-  return ieo_none();
-}
-
-IEO_PURE IeoResult
-ieo_none_sub(IEO_NOTNULL const IeoTerm *self, IEO_NOTNULL const IeoTerm *other)
-{
-  ieo_assert(IEO_OK(ieo_is_none(self)));
-  IEO_TRY_(ieo_is_none(other));
-  return ieo_none();
-}
-
-IEO_PURE IeoResult
-ieo_none_eq(IEO_NOTNULL const IeoTerm *self, IEO_NOTNULL const IeoTerm *other)
+static IEO_PURE IeoResult
+eq_method(const IeoTerm *self, const IeoTerm *other)
 {
   return IEO_BOOL(IEO_OK(ieo_is_none(self)) && IEO_OK(ieo_is_none(other)));
 }
 
-IEO_PURE IeoResult
-ieo_none_neq(IEO_NOTNULL const IeoTerm *self, IEO_NOTNULL const IeoTerm *other)
+static IEO_PURE IeoResult
+neq_method(const IeoTerm *self, const IeoTerm *other)
 {
   return IEO_BOOL(IEO_ERR(ieo_is_none(self)) || IEO_ERR(ieo_is_none(other)));
 }
 
-IEO_PURE IeoResult
-ieo_none_compare(IEO_NOTNULL const IeoTerm *self,
-                 IEO_NOTNULL const IeoTerm *other)
+static IEO_PURE IeoResult
+compare_method(const IeoTerm *self, const IeoTerm *other)
 {
   ieo_assert(IEO_OK(ieo_is_none(self)));
   ieo_assert(IEO_OK(ieo_is_none(other)));
@@ -94,12 +69,12 @@ IeoType ieo_std_type_none = {
   .type_name = &ieo_none_type_name,
   .term_size = sizeof(IeoNone),
   .deleter = ieo_term_deleter,
-  .neg_func = ieo_none_neg,
-  .add_func = ieo_none_add,
-  .div_func = ieo_none_div,
-  .mul_func = ieo_none_mul,
-  .sub_func = ieo_none_sub,
-  .eq_func = ieo_none_eq,
-  .neq_func = ieo_none_neq,
-  .compare_func = ieo_none_compare,
+  .neg_func = unary_math_method,
+  .add_func = binary_math_method,
+  .div_func = binary_math_method,
+  .mul_func = binary_math_method,
+  .sub_func = binary_math_method,
+  .eq_func = eq_method,
+  .neq_func = neq_method,
+  .compare_func = compare_method,
 };

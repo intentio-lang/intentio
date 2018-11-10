9393542da17147debfb32983afb5df4315877c84
Marek Kaput <marek.kaput@outlook.com>
2018-11-10 23:38:49 +0100

    better error for not implemented Imp transform

--- compiler/src/Intentio/Codegen/Imp/Transform.hs
+++ compiler/src/Intentio/Codegen/Imp/Transform.hs
@@ -6,8 +6,10 @@ where
 import           Intentio.Prelude
 
 import qualified Intentio.Codegen.Imp.Model    as Imp
-import           Intentio.Compiler              ( CompilePure )
+import           Intentio.Compiler              ( CompilePure
+                                                , pushIceFor
+                                                )
 import qualified Intentio.Hir                  as Hir
 
 impTransform :: Hir.Body () -> CompilePure (Imp.Body ())
-impTransform = undefined
+impTransform body = pushIceFor body "Imp Transform not implemented."

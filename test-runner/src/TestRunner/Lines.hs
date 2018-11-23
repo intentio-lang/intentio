862b37f804f96b1a9e4c34e3fdc550fb496ef36f
Marek Kaput <marek.kaput@outlook.com>
2018-11-23 21:43:04 +0100

    Show working directory in test runner logs where it makes sens

---
+++ test-runner/src/TestRunner/Lines.hs
@@ -0,0 +1,17 @@
+module TestRunner.Lines where
+
+import           Intentio.Prelude
+
+import qualified Data.Text                     as T
+
+newtype Lines = Lines {unlines :: Text}
+
+instance Semigroup Lines where
+  Lines a <> Lines b = Lines $ a <> "\n" <> b
+
+instance Monoid Lines where
+  mempty = Lines ""
+  mconcat = Lines . T.unlines . fmap unlines
+
+tellLine :: (MonadWriter Lines m) => Text -> m ()
+tellLine = tell . Lines
